457559752756963f64f0656a9c1b92a6500a36d9
Edoardo Oglietti <59146921+00-uno-00@users.noreply.github.com>
2024-06-12 17:58:39 +0200

    update express server working with static pages

--- solution/client_server/routes/index.js
+++ solution/client_server/routes/index.js
@@ -1,21 +1,10 @@
 var express = require('express');
+const {join} = require("path");
 var router = express.Router();
-var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
 });
-
-router.get('/contatti', function(req, res) {
-  championship = req.body.championship;
-  // Get teams from database
-  res.json(teams);
-  res.render('contatti', { title: 'Contatti' });
-})
-
-router.get('/stats_match.html', function(req, res) {
-  res.sendFile(join(__dirname,'..','public','stats_match.html'));
-})
 
 module.exports = router;
